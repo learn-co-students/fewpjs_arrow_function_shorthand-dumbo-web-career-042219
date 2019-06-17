e834f8fe235624b87fb463a4769ceeb80a87d02e
Miles Marillo <mjm03@hampshire.edu>
2019-06-17 09:55:02 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = () => {
+	let result = (2000 / 100);
+	return result
+}
+
+const square = (num) => {
+	return (num * num)
+}
+
+const add = (num1, num2) => {
+	return (num1+ num2)
+}
